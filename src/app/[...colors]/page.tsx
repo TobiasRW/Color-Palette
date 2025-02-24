c0d4c3426220e0927bb2f1f0432ccfc30efe4d73
TobiasRW <tobiasrw98@gmail.com>
2025-02-24 17:47:14 +0100

    text color based on generated color

--- src/app/[...colors]/page.tsx
+++ src/app/[...colors]/page.tsx
@@ -7,6 +7,15 @@ type PageProps = {
   }>;
 };
 
+const determineTextColor = (color: string) => {
+  const r = parseInt(color.substring(0, 2), 16);
+  const g = parseInt(color.substring(2, 4), 16);
+  const b = parseInt(color.substring(4, 6), 16);
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return brightness >= 128 ? "black" : "white";
+};
+
 export default async function ColorsPage({ params }: PageProps) {
   const { colors: paramColors } = await params;
 
@@ -19,7 +28,10 @@ export default async function ColorsPage({ params }: PageProps) {
         <div
           key={color}
           className="flex items-center justify-center text-white font-semibold h-full"
-          style={{ backgroundColor: `#${color}` }}
+          style={{
+            backgroundColor: `#${color}`,
+            color: determineTextColor(color),
+          }}
         >
           <p>{color.toUpperCase()}</p>
         </div>
