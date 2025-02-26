8871dda052b9a47ebe058df8f1fceed6882616a3
TobiasRW <tobiasrw98@gmail.com>
2025-02-26 16:36:48 +0100

    hide header on [...corlors]

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,15 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  // Check if the path contains hex colors (for the generate colors page)
+  const isColorPath = /^\/[0-9a-f-]+$/i.test(pathname || "");
+
+  // If we're on a color path, don't render the header
+  if (isColorPath) return null;
+
   return (
     <header className="border-b border-foreground bg-background p-3">
       <h1 className="text-3xl font-bold text-orange">
